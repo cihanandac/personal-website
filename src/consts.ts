8d31c79518125cd918fa512099bf11d96a323a45
Cihan Andac <cihanandac@gmail.com>
2026-08-10 01:36:36 +0200

    fix: temporarily switch of the site indexing

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,7 @@
 
 export const SITE_TITLE = 'Astro Blog';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+
+// Set to true once the placeholder content has been replaced. While false, every
+// page emits `noindex` so search engines don't index the starter template.
+export const SITE_INDEXABLE = false;
