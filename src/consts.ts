a139cac75488416d5cd1f65c2e3710077489676f
Cihan Andac <cihanandac@gmail.com>
2026-08-10 02:01:17 +0200

    chore: remove boilerplate content

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Cihan Andac';
+export const SITE_DESCRIPTION = 'Personal site and notes by Cihan Andac.';
 
 // Set to true once the placeholder content has been replaced. While false, every
 // page emits `noindex` so search engines don't index the starter template.
